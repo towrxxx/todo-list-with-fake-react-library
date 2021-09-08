20452c748905e1d0875851283d5f268b2ed3e5a9
hairrrr <haidv.b17cn199@stu.ptit.edu.vn>
2021-09-08 23:48:24 +0700

    second-commit

--- store.js
+++ store.js
@@ -1,9 +1,9 @@
 /* eslint-disable import/extensions */
 import { createStore } from "./core.js";
 import reducer from "./reducer.js";
-import withLogger from "./logger.js";
+// import withLogger from "./logger.js";
 
-const { attach, dispatch, connect } = createStore(withLogger(reducer));
+const { attach, dispatch, connect } = createStore(reducer);
 
 window.dispatch = dispatch;
 
